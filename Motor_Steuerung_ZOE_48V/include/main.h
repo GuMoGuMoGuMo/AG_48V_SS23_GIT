1d9b48cb186433e475ac2bde5b0f333e0a8c6348
Philipp <philminusmay@gmail.com>
2023-03-22 09:42:17 +0100

    Code optimierung und aufräumen

--- Motor_Steuerung_ZOE_48V/include/main.h
+++ Motor_Steuerung_ZOE_48V/include/main.h
@@ -1,15 +1,10 @@
 
 // include
-#include <Arduino.h>
-#include "ACS712.h"
-#include <PID_v1.h>
-#include <movingAvg.h>
-#include <Ewma.h>
-
+#include <Arduino.h> // Arduino standard lib
 
 // Pinning
 #define PWM_ERREGUNG 13
-#define U_I_ERREGUNG_SENS A0
+#define I_ERREGUNG_SENS A0
 #define U_POTI A1
 #define U_BATT_TRAKTION A2
 
@@ -17,6 +12,10 @@
 #define R_POT100 4700 // Ohm
 unsigned short av_R_poti;
 float r_poti;
+
+//Messschaltung
+#define R1_span_teil 100 // Ohm
+#define R2_span_teil 100 // Ohm
 
 // Motordaten
 float R_ROTOR = 8;//8; //Ohm - zunächst als konstant angenommen, kann z.B. im Setup mit einem 100% DC vermessen werden
@@ -28,27 +27,29 @@ float I_Erregung_MAX = u_batt_traktion/R_ROTOR; //A zunächst als konstant angen
 float dc_pwm;
 
 // PID Erregerstrom
-double Setpoint;
-double Input;
-double Output;
-double Kp=6;
-double Ki=2; 
-double Kd=0.001;
-PID erregerstrom_pid(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+#include <PID_v1.h> // PID Regler lib
+double Kp_erregerstrom = 6;
+double Ki_erregerstrom = 2; 
+double Kd_erregerstrom = 0.001;
+double erregerstrom_stell,erregerstrom_sensor,erregerstrom_soll;
+PID erregerstrom_pid(&erregerstrom_sensor, &erregerstrom_stell, &erregerstrom_soll, Kp_erregerstrom, Ki_erregerstrom, Kd_erregerstrom, DIRECT); //init PID Objekt
 
-float erregerstrom_stell,erregerstrom_sensor,erregerstrom_soll;
 
 // Stromsensor
-#define VCC_STROM_SENSOR 5.00 // Volt
-#define FAKTOR_STROM_SENSOR 0.02 // 20mV/A Umrechungsfaktor
-ACS712  ACS(A0, 5.0, 1023, 20);
+#include <ACS712.h> // Stromsensor lib
 
-float value_low_pass=0;
+ACS712  ACS(I_ERREGUNG_SENS, 5.0, 1023, 20); // init Stomsensor Objekt: PIN, VCC, ADC Auflösung, 20mV/A
 
+// Filter
+#include <Ewma.h> // exp weighted moving avg filter liv
+#include <RunningAverage.h> // moving avg filter lib
 
-//average filter
+RunningAverage erreger_strom_mov_avg(10); // init moving average filter, 10 = Anzahl der Messungen für Filter
+Ewma we_avg_Filter1(0.2);   // Less smoothing - faster to detect changes, but more prone to noise
+Ewma we_avg_Filter2(0.01);  // More smoothing - less prone to noise, but slower to detect changes
+float erreger_strom_low_pass=0; // init TP filter
+float weight = 0.2; // Gewichtung TP filter
 
-RunningAverage erreger_strom_avg(10);
 
 // Debug Optionen
 
