760cf13531517e1e9e1936dbbeacac969058e81a
Philipp <philminusmay@gmail.com>
2023-03-22 08:28:33 +0100

    Improved Controller Design, added libraries

--- Motor_Steuerung_ZOE_48V/include/main.h
+++ Motor_Steuerung_ZOE_48V/include/main.h
@@ -1,38 +1,54 @@
 
 // include
 #include <Arduino.h>
+#include "ACS712.h"
+#include <PID_v1.h>
+#include <movingAvg.h>
+#include <Ewma.h>
+
 
 // Pinning
 #define PWM_ERREGUNG 13
 #define U_I_ERREGUNG_SENS A0
 #define U_POTI A1
+#define U_BATT_TRAKTION A2
 
-// Globale Variablen
-unsigned short av_R_poti;
-
-float R_ROTOR = 8; //Ohm - zunächst als konstant angenommen, kann z.B. im Setup mit einem 100% DC vermessen werden
-float L_ROTOR = 0.4; //H Messung von Herr Zirn 03.2022
-float U_BATT_TRAKTION_48V = 48; //V
-float I_Erregung_MAX = U_BATT_TRAKTION_48V/R_ROTOR; //A zunächst als konstant angenommen
-
-unsigned short dc_pwm;
-
-unsigned long last_time;
-float regeldifferenz_alt = 0;
-float erregerstrom_sensor = 0;
-float erregerstrom_soll = 0;
-float erregerstrom_stell = 0;
-float integral_erregerstrom_regelung = 0;
-
-
-// Konstanten
+// Potentiometer
 #define R_POT100 4700 // Ohm
+unsigned short av_R_poti;
+float r_poti;
+
+// Motordaten
+float R_ROTOR = 8;//8; //Ohm - zunächst als konstant angenommen, kann z.B. im Setup mit einem 100% DC vermessen werden
+float L_ROTOR = 0.4; //H Messung von Herr Zirn 03.2022
+float u_batt_traktion = 13; //V
+float I_Erregung_MAX = u_batt_traktion/R_ROTOR; //A zunächst als konstant angenommen
+
+// PWM Signal Erregerstrom
+float dc_pwm;
+
+// PID Erregerstrom
+double Setpoint;
+double Input;
+double Output;
+double Kp=6;
+double Ki=2; 
+double Kd=0.001;
+PID erregerstrom_pid(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
+float erregerstrom_stell,erregerstrom_sensor,erregerstrom_soll;
+
+// Stromsensor
 #define VCC_STROM_SENSOR 5.00 // Volt
 #define FAKTOR_STROM_SENSOR 0.02 // 20mV/A Umrechungsfaktor
+ACS712  ACS(A0, 5.0, 1023, 20);
 
-#define Erregerstrom_Regelung_KP 50
-#define Erregerstrom_Regelung_KI 10
-#define Erregerstrom_Regelung_KD 0
+float value_low_pass=0;
+
+
+//average filter
+
+RunningAverage erreger_strom_avg(10);
 
 // Debug Optionen
 
@@ -45,81 +61,3 @@ float integral_erregerstrom_regelung = 0;
   #define DEBUG_PRINTLN(x)
   #define DEBUG_PRINT(x) 
 #endif
-
-// Setup Funktionen
-void init_potentiometer(){
-  pinMode(U_POTI,INPUT); // initialize iputs/outputs
-}
-
-void init_pwm_i_erregung(){
-    pinMode(PWM_ERREGUNG,OUTPUT); // initialize iputs/outputs
-}
-
-void init_strom_sensor(){
-    pinMode(U_I_ERREGUNG_SENS,INPUT); // initialize iputs/outputs
-}
-
-void init_erregerstrom_regelung(){
-    last_time = micros();
-}
-
-// Loop Funktionen
-void loop_potentiometer(){
-  av_R_poti = analogRead(U_POTI); // responds with value between 0..1023 
-  DEBUG_PRINT("R_Poti= ");DEBUG_PRINT(av_R_poti/1024.0*R_POT100);DEBUG_PRINTLN(" Ohm"); // calculates R
-  erregerstrom_soll = av_R_poti/1024.0*I_Erregung_MAX;
-}
-
-void loop_pwm_i_erregung(){   
-    dc_pwm = round(erregerstrom_stell/I_Erregung_MAX);
-    analogWrite(PWM_ERREGUNG,dc_pwm*255.0/100); // frequ= 980 Hz Value = DC 0...255
-    DEBUG_PRINT("DC_PWM= ");DEBUG_PRINT(dc_pwm);DEBUG_PRINTLN(" %");
-}
-
-void loop_strom_sensor(){
-    float u_erregerstrom_sensor;
-    u_erregerstrom_sensor = (5.0/1023.0)*analogRead(U_I_ERREGUNG_SENS); // Spannungssignal einlesen
-    u_erregerstrom_sensor = u_erregerstrom_sensor - (VCC_STROM_SENSOR * 0.5) + 0.007 ;// 0.007 Wert um Spannug 0 zu setzen wenn I Errergung = 0 ist
-    erregerstrom_sensor = u_erregerstrom_sensor/FAKTOR_STROM_SENSOR; // Umrechnung in Strom
-    DEBUG_PRINT("Erregerstrom_Sensor: ");DEBUG_PRINT(erregerstrom_sensor);DEBUG_PRINTLN(" A");
-}
-
-void loop_erregerstrom_regelung(){
-    float regelanteil_p;
-    float regelanteil_i;
-    float regelanteil_d;
-    
-    float fuehrungsgroesse = erregerstrom_soll;
-    float regelgroesse = erregerstrom_sensor; 
-    float stellgroesse = erregerstrom_stell;
-
-    float regeldifferenz;
-    float delta_regeldifferenz;
-    
-    float integral = integral_erregerstrom_regelung;
-    float ableitung;
-    
-    unsigned long delta_t;
-
-    delta_t = micros()-last_time;
-    last_time = micros();
-    
-    regeldifferenz = fuehrungsgroesse - regelgroesse;
-    
-    delta_regeldifferenz = regeldifferenz - regeldifferenz_alt;
-    regeldifferenz_alt = regeldifferenz;
-    
-    integral += regeldifferenz * delta_t;
-    integral_erregerstrom_regelung = integral;
-
-    ableitung = delta_regeldifferenz / delta_t;
-
-    regelanteil_p = Erregerstrom_Regelung_KP * regeldifferenz;
-    regelanteil_i = Erregerstrom_Regelung_KI * integral;
-    regelanteil_d = Erregerstrom_Regelung_KD * ableitung;
-    
-    stellgroesse = regelanteil_p + regelanteil_i + regelanteil_d;
-    
-    erregerstrom_stell = constrain(stellgroesse,0,I_Erregung_MAX);
-    DEBUG_PRINT("Erregerstrom_Stell: ");DEBUG_PRINT(erregerstrom_stell);DEBUG_PRINTLN(" A");
-}
--- Motor_Steuerung_ZOE_48V/src/main.cpp
+++ Motor_Steuerung_ZOE_48V/src/main.cpp
@@ -1,22 +1,92 @@
 #include "main.h"
 
+// Setup Funktionen
+void init_potentiometer(){
+  pinMode(U_POTI,INPUT); // initialize iputs/outputs
+}
 
+void init_pwm_i_erregung(){
+    pinMode(PWM_ERREGUNG,OUTPUT); // initialize iputs/outputs
+}
+
+void init_strom_sensor(){
+    pinMode(U_I_ERREGUNG_SENS,INPUT); // initialize iputs/outputs
+}
+
+void init_messung_u_batt_traktion(){
+    pinMode(U_BATT_TRAKTION,INPUT);
+    u_batt_traktion = analogRead(U_BATT_TRAKTION); 
+}
+
+
+// Loop Funktionen
+void loop_potentiometer(){
+  av_R_poti = analogRead(U_POTI); // responds with value between 0..1023 
+  r_poti = av_R_poti/1024.0*R_POT100;
+  DEBUG_PRINT(">R_Poti: ");DEBUG_PRINT(r_poti);DEBUG_PRINTLN(" Ohm"); // calculates R
+  erregerstrom_soll = av_R_poti/1024.0*I_Erregung_MAX;
+  DEBUG_PRINT(">Erregerstrom_soll: ");DEBUG_PRINT(erregerstrom_soll);DEBUG_PRINTLN(" A"); // calculates R  
+}
+
+void loop_pwm_i_erregung(){   
+    //dc_pwm = constrain(erregerstrom_stell/I_Erregung_MAX,0,1);
+    dc_pwm = 0.1;
+    analogWrite(PWM_ERREGUNG,dc_pwm*255.0); // frequ= 980 Hz Value = DC 0...255
+    DEBUG_PRINT(">DC_PWM: ");DEBUG_PRINT(dc_pwm);DEBUG_PRINTLN(" %");
+}
+
+movingAvg strom_movingAvg(10);
+
+Ewma we_avg_Filter1(0.2);   // Less smoothing - faster to detect changes, but more prone to noise
+Ewma we_avg_Filter2(0.01);  // More smoothing - less prone to noise, but slower to detect changes
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   
   init_potentiometer();
-  init_strom_sensor();
-  init_erregerstrom_regelung();
+  //init_strom_sensor();
   init_pwm_i_erregung();
+
+  strom_movingAvg.begin();
+  ACS.autoMidPoint();
+
+  erreger_strom_avg.clear(); // explicitly start clean
+
+  Input = 0;
+  Setpoint = 0;
+
+  //turn the PID on
+  erregerstrom_pid.SetOutputLimits(0, I_Erregung_MAX);
+  erregerstrom_pid.SetMode(AUTOMATIC);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
   loop_potentiometer();
-  loop_strom_sensor();
-  loop_erregerstrom_regelung();
+  //erregerstrom_soll = 0.5*sin(millis()/1000.0)+0.5; //A
+  //DEBUG_PRINT(">Erregerstrom_soll: ");DEBUG_PRINT(erregerstrom_soll);DEBUG_PRINTLN(" A");
+
+  int mA = ACS.mA_DC();
+  DEBUG_PRINT(">Strom: ");DEBUG_PRINT(mA/1000.0);DEBUG_PRINTLN(" A");
+  erreger_strom_avg.addValue(mA/1000.0);
+  DEBUG_PRINT(">Erregerstrom_Sensor_mov_AVG: ");DEBUG_PRINT(erreger_strom_avg.getAverage());DEBUG_PRINTLN(" A");
+  
+  float weight = 0.2;
+  value_low_pass += weight * (mA - value_low_pass);  // low pass filtering
+  DEBUG_PRINT(">Erregerstrom_Sensor_lowpass: ");DEBUG_PRINT(value_low_pass/1000.0);DEBUG_PRINTLN(" A");
+
+  float value_we_avg_Filter1 = we_avg_Filter1.filter(mA);
+  DEBUG_PRINT(">Erregerstrom_Sensor_we_mvavg1: ");DEBUG_PRINT(value_we_avg_Filter1/1000.0);DEBUG_PRINTLN(" A");
+
+  float value_we_avg_Filter2 = we_avg_Filter2.filter(mA);
+  DEBUG_PRINT(">Erregerstrom_Sensor_we_mvavg2: ");DEBUG_PRINT(value_we_avg_Filter2/1000.0);DEBUG_PRINTLN(" A");
+
+  Setpoint = erregerstrom_soll;
+  Input = erreger_strom_avg.getAverage();
+  erregerstrom_pid.Compute();
+  erregerstrom_stell = Output;
+  DEBUG_PRINT(">Erregerstrom_stell: ");DEBUG_PRINT(erregerstrom_stell);DEBUG_PRINTLN(" A");
   loop_pwm_i_erregung();
 }
