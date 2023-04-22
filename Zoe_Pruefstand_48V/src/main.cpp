130c74f257f0cc881e7ab25f6be189d98daf9f7a
Philipp <philminusmay@gmail.com>
2023-04-22 10:05:43 +0200

    include Pinning, most objects are defined

--- Zoe_Pruefstand_48V/src/main.cpp
+++ Zoe_Pruefstand_48V/src/main.cpp
@@ -8,73 +8,77 @@
 
 #include <SPI.h>
 
-// define pins
-#ifdef ESP8266
-   #define STMPE_CS 16
-   #define TFT_CS   0
-   #define TFT_DC   15
-   #define SD_CS    2
-#elif defined(ESP32) && !defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S2)
-   #define STMPE_CS 32
-   #define TFT_CS   15
-   #define TFT_DC   33
-   #define SD_CS    14
-#elif defined(TEENSYDUINO)
-   #define TFT_DC   10
-   #define TFT_CS   4
-   #define STMPE_CS 3
-   #define SD_CS    8
-#elif defined(ARDUINO_STM32_FEATHER)
-   #define TFT_DC   PB4
-   #define TFT_CS   PA15
-   #define STMPE_CS PC7
-   #define SD_CS    PC5
-#elif defined(ARDUINO_NRF52832_FEATHER)  /* BSP 0.6.5 and higher! */
-   #define TFT_DC   11
-   #define TFT_CS   31
-   #define STMPE_CS 30
-   #define SD_CS    27
-#elif defined(ARDUINO_MAX32620FTHR) || defined(ARDUINO_MAX32630FTHR)
-   #define TFT_DC   P5_4
-   #define TFT_CS   P5_3
-   #define STMPE_CS P3_3
-   #define SD_CS    P3_2
-#else
-    // Anything else, defaults!
-   #define STMPE_CS 6
-   #define TFT_CS   9
-   #define TFT_DC   10
-   #define SD_CS    5
-#endif
+// define ILI9341+Touchscreen
+#include <SPI.h>
+#include <stdint.h>
+#include <TouchScreen.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
 
+#define TFT_DC 32
+#define TFT_CS 30
+#define YP A6  // must be an analog pin, use "An" notation!
+#define XM A7  // must be an analog pin, use "An" notation!
+#define YM 34   // can be a digital pin
+#define XP 32   // can be a digital pin
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+
+//Analog/Digital Pins
+#define EXCITATION_CURRENT_POTI_ZOE_PIN A0
+#define POTI_THROTTLE_DMC_PIN A1
+#define POTI_BRAKE_DMC_PIN A2
+
+#define POTI_EGAS_DMC_PIN A5
+#define SWITCH_EGAS_DMC_PIN 28
+
+#define FOOT_SWITCH_DMC_PIN 26
+
+#define PWM_ERREGUNG_ZOE_PIN A13
+
+#define FOOT_SWITCH_KELLY_PIN 22
+#define BRAKE_SWITCH_KELLY_PIN 24
+
+#define POTI_THROTTLE_KELLY_PIN A3
+#define POTI_BRAKE_KELLY_PIN A4
+
+// Analog-Digital Converter ADS1115
+#include "ADS1X15.h"
+#define ADRESS_ADC_VEHICLE_DMZ_ZOE 0x48
+#define ADRESS_ADC_MEASURING_SHAFT_ZOE 0x49
+
+ADS1115 adc_vehicle_dmc_zoe(ADRESS_ADC_VEHICLE_DMZ_ZOE);
+ADS1115 adc_measuring_shaft(ADRESS_ADC_MEASURING_SHAFT_ZOE);
+
+// ADC DMC PINS 
 #define BATTERY_VOLTAGE_SENSOR_PIN 1  
-#define BATTERY_CURRENT_SENSOR_PIN 1
+#define BATTERY_CURRENT_SENSOR_PIN 2
+#define EXCITATION_CURRENT_SENSOR_ZOE_PIN 3
 
-#define FOOT_SWITCH_DMC_PIN 1
-#define BRAKE_SWITCH_DMC_PIN 1
-#define PWM_ERREGUNG_ZOE_PIN 1
-
-#define EXCITATION_CURRENT_SENSOR_ZOE_PIN 1
-
-#define EXCITATION_CURRENT_POTI_ZOE_PIN 1
-#define GAS_POTI_ZOE_PIN 1
-#define BREMS_POTI_ZOE_PIN 1
-
-#define FOOT_SWITCH_KELLY_PIN 1
-#define BRAKE_SWITCH_KELLY_PIN 1
-
-#define GAS_POTI_KELLY_PIN 1
-#define BREMS_POTI_KELLY_PIN 1
-
-
+// ADC measuring shaft PINS
+ #define TORQUE_MEASURING_SHAFT_PIN 2
+ #define SPEED_MEASURING_SHAFT_PIN 3
 
 // define constants
-#define R_ERREGERCOIL 1
-#define U_REF 1
-#define R1_VOLTAGE_DIVIDER 1
-#define R2_VOLTAGE_DIVIDER 1
-
+#define R_EXCITATION_COIL 1
+#define U_REF 5
+#define R1_VOLTAGE_DIVIDER 100000
+#define R2_VOLTAGE_DIVIDER 5600
 // define variables
+
+// dacs
+#include <Adafruit_MCP4725.h>
+
+#define ADRESS_DAC_gas_dmc 0x62
+#define ADRESS_DAC_bremse_dmc 0x63
+#define ADRESS_DAC_gas_kelly 0x60
+#define ADRESS_DAC_bremse_kelly 0x61
+Adafruit_MCP4725 dac_gas_dmc;
+Adafruit_MCP4725 dac_bremse_dmc;
+Adafruit_MCP4725 dac_gas_kelly;
+Adafruit_MCP4725 dac_bremse_kelly;
+
 
 // create objects 
 
@@ -107,11 +111,11 @@ struct motor_control {
   int torque_max;
   int speed_max;
 
-  int gaspoti_sensor;
-  int gaspoti_sensor_t_minus_1;
+  int throttle_poti_sensor;
+  int throttle_poti_sensor_t_minus_1;
 
-  int bremspoti_sensor;
-  int bremspoti_sensor_t_minus_1;
+  int brake_poti_sensor;
+  int brake_poti_sensor_t_minus_1;
 
   int excitation_current_poti_sensor;
   int excitation_current_poti_sensor_t_minus_1;
@@ -125,7 +129,7 @@ struct motor_control {
   double torque_ist;
   double torque_ist_t_minus_1;
 
-  double drehzahl_output;
+  double speed_output;
   double torque_output;
   
   double excitation_current_ist;
@@ -164,20 +168,6 @@ struct motor_control motor_control_dmc_zoe;
 struct motor_control motor_control_kelly_pmac;
 struct measurement measuring_shaft; 
 
-// display
-#include <Adafruit_GFX.h>
-#include <Adafruit_ILI9341.h>
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
-
-// adcs Analog-Digital Converter ADS1115
-#include "ADS1X15.h"
-#define ADRESS_ADC_VEHICLE_DMZ_ZOE 0x48
-#define ADRESS_ADC_MEASURING_SHAFT_ZOE 0x49
-
-ADS1115 adc_vehicle_dmc_zoe(ADRESS_ADC_VEHICLE_DMZ_ZOE);
-ADS1115 adc_measuring_shaft(ADRESS_ADC_MEASURING_SHAFT_ZOE);
-
-
 // current sensors
 #include <ACS712.h> // Stromsensor lib
 ACS712 battery_current_sensor(BATTERY_CURRENT_SENSOR_PIN, U_REF, 1023,66); // init Stomsensor Objekt: PIN, VCC, ADC Auflösung, 66mV/A
@@ -200,23 +190,11 @@ uint16_t read_adc_excitation_current_sensor(ADS1115 adc) {
 };
 
 
-// dacs
-#include <Adafruit_MCP4725.h>
-
-#define ADRESS_DAC_gas_dmc 0x60
-#define ADRESS_DAC_bremse_dmc 0x61
-#define ADRESS_DAC_gas_kelly 0x62
-#define ADRESS_DAC_bremse_kelly 0x63
-Adafruit_MCP4725 dac_gas_dmc;
-Adafruit_MCP4725 dac_bremse_dmc;
-Adafruit_MCP4725 dac_gas_kelly;
-Adafruit_MCP4725 dac_bremse_kelly;
-
 // create pid controller
 #include <PID_v1.h> 
-PID motor1_speed_pid(&measuring_shaft.drehzahl_messwelle_sensor, &motor_control_dmc_zoe.drehzahl_output, &motor_control_dmc_zoe.speed_setpoint, motor_control_dmc_zoe.kp_speed, motor_control_dmc_zoe.ki_speed, motor_control_dmc_zoe.kd_speed, DIRECT);
+PID motor1_speed_pid(&measuring_shaft.drehzahl_messwelle_sensor, &motor_control_dmc_zoe.speed_output, &motor_control_dmc_zoe.speed_setpoint, motor_control_dmc_zoe.kp_speed, motor_control_dmc_zoe.ki_speed, motor_control_dmc_zoe.kd_speed, DIRECT);
 PID excitation_current_pid(&motor_control_dmc_zoe.excitation_current_ist, &motor_control_dmc_zoe.excitation_current_output, &motor_control_dmc_zoe.exication_current_setpoint, motor_control_dmc_zoe.kp_excitation_current, motor_control_dmc_zoe.ki_excitation_current, motor_control_dmc_zoe.kd_excitation_current, DIRECT);
-PID motor2_speed_pid(&measuring_shaft.drehzahl_messwelle_sensor, &motor_control_kelly_pmac.drehzahl_output, &motor_control_kelly_pmac.speed_setpoint, motor_control_kelly_pmac.kp_speed, motor_control_kelly_pmac.ki_speed, motor_control_kelly_pmac.kd_speed, DIRECT);
+PID motor2_speed_pid(&measuring_shaft.drehzahl_messwelle_sensor, &motor_control_kelly_pmac.speed_output, &motor_control_kelly_pmac.speed_setpoint, motor_control_kelly_pmac.kp_speed, motor_control_kelly_pmac.ki_speed, motor_control_kelly_pmac.kd_speed, DIRECT);
 
 
 
@@ -234,9 +212,9 @@ void test_bench_task(test_bench test_bench, motor_control motor_control) {
     if (test_bench.mode) { // mode=1 automatic; mode=0 manual
         // hier code mit tabelle prüfgramm zeit sollmoment solldrezahl
     }
-    motor_control_dmc_zoe.speed_setpoint = (motor_control_dmc_zoe.gaspoti_sensor-motor_control_dmc_zoe.bremspoti_sensor)/100.0*motor_control_dmc_zoe.speed_max;
+    motor_control_dmc_zoe.speed_setpoint = (motor_control_dmc_zoe.throttle_poti_sensor-motor_control_dmc_zoe.brake_poti_sensor)/100.0*motor_control_dmc_zoe.speed_max;
     motor_control_dmc_zoe.exication_current_setpoint = motor_control_dmc_zoe.excitation_current_poti_sensor/100.0*motor_control.excitation_current_max;
-    motor_control_kelly_pmac.speed_setpoint = (motor_control_kelly_pmac.gaspoti_sensor-motor_control_kelly_pmac.bremspoti_sensor)/100.0*motor_control_kelly_pmac.speed_max;
+    motor_control_kelly_pmac.speed_setpoint = (motor_control_kelly_pmac.throttle_poti_sensor-motor_control_kelly_pmac.brake_poti_sensor)/100.0*motor_control_kelly_pmac.speed_max;
 }
 
 void vehicle_task(vehicle vehicle) {
@@ -251,7 +229,7 @@ void vehicle_task(vehicle vehicle) {
 void motor1_control_task(motor_control motor_control, vehicle vehicle) {
     // motor1_control_task
     // calculate max excitation current, torque, speed
-    motor_control.excitation_current_max = round(vehicle.battery_voltage/R_ERREGERCOIL);
+    motor_control.excitation_current_max = round(vehicle.battery_voltage/R_EXCITATION_COIL);
     motor_control.torque_max; // put code here to calculate max torque
     motor_control.speed_max; // put code here to calculate max speed
     //read excitation_current
@@ -259,9 +237,9 @@ void motor1_control_task(motor_control motor_control, vehicle vehicle) {
     // read excitation_current_poti
     motor_control.excitation_current_poti_sensor = round(analogRead(EXCITATION_CURRENT_POTI_ZOE_PIN)/1024.0*100.0);
     //read gas_poti
-    motor_control.gaspoti_sensor = round(analogRead(GAS_POTI_ZOE_PIN)/1024.0*100.0); // 0...100
+    motor_control.throttle_poti_sensor = round(analogRead(POTI_THROTTLE_DMC_PIN)/1024.0*100.0); // 0...100
     //read bremse_poti
-    motor_control.bremspoti_sensor = round(analogRead(BREMS_POTI_ZOE_PIN)/1024.0*100.0); // 0...100
+    motor_control.brake_poti_sensor = round(analogRead(POTI_BRAKE_DMC_PIN)/1024.0*100.0); // 0...100
     
 
     // pid excitation_current
@@ -274,15 +252,15 @@ void motor1_control_task(motor_control motor_control, vehicle vehicle) {
     motor1_speed_pid.Compute();
 
     // set dac gas/brake
-    int prozent = round(abs(motor_control.drehzahl_output)/motor_control.speed_max*100.0); //= calculate percentage of gas/brake applied here;
+    int prozent = round(abs(motor_control.speed_output)/motor_control.speed_max*100.0); //= calculate percentage of gas/brake applied here;
     uint16_t output = round(prozent/100.0*4096.0); // Prozent 0..100
-    if (motor_control.drehzahl_output>=0) {
+    if (motor_control.speed_output>=0) {
       dac_gas_dmc.setVoltage(abs(output),false);
       dac_bremse_dmc.setVoltage(0,false);
       motor_control.state_foot_switch = 1;
       motor_control.state_brake_switch = 0;
     }
-    else { // if drehzahl_output <0
+    else { // if speed_output <0
       dac_gas_dmc.setVoltage(0,false);
       dac_bremse_dmc.setVoltage(abs(output),false);
       motor_control.state_foot_switch = 1;
@@ -291,35 +269,33 @@ void motor1_control_task(motor_control motor_control, vehicle vehicle) {
 
     // set foot switch
     digitalWrite(FOOT_SWITCH_DMC_PIN,motor_control.state_foot_switch);
-    // set brake switch
-    digitalWrite(BRAKE_SWITCH_DMC_PIN,motor_control.state_brake_switch);
 }
 
 void motor2_control_task(motor_control motor_control, vehicle vehicle) {
   // motor2_control_task
     // calculate max excitation current, torque, speed
-    motor_control.excitation_current_max = round(vehicle.battery_voltage/R_ERREGERCOIL);
+    motor_control.excitation_current_max = round(vehicle.battery_voltage/R_EXCITATION_COIL);
     motor_control.torque_max; // put code here to calculate max torque
     motor_control.speed_max; // put code here to calculate max speed
   
     //read gas_poti
-    motor_control.gaspoti_sensor = round(analogRead(GAS_POTI_KELLY_PIN)/1024.0*100.0); // 0...100
+    motor_control.throttle_poti_sensor = round(analogRead(POTI_THROTTLE_KELLY_PIN)/1024.0*100.0); // 0...100
     //read bremse_poti
-    motor_control.bremspoti_sensor = round(analogRead(BREMS_POTI_KELLY_PIN)/1024.0*100.0); // 0...100
+    motor_control.brake_poti_sensor = round(analogRead(POTI_BRAKE_KELLY_PIN)/1024.0*100.0); // 0...100
     
     // pid motor speed
     motor1_speed_pid.Compute();
 
     // set dac gas/brake
-    int prozent = round(abs(motor_control.drehzahl_output)/motor_control.speed_max*100.0); // calculate percentage of gas/brake applied;
+    int prozent = round(abs(motor_control.speed_output)/motor_control.speed_max*100.0); // calculate percentage of gas/brake applied;
     uint16_t output = round(prozent/100.0*4096.0); // prozent 0..100
-    if (motor_control.drehzahl_output>=0) {
+    if (motor_control.speed_output>=0) {
       dac_gas_kelly.setVoltage(abs(output),false);
       dac_bremse_kelly.setVoltage(0,false);
       motor_control.state_foot_switch = 1;
       motor_control.state_brake_switch = 0;
     }
-    else { // if drehzahl_output <0
+    else { // if speed_output <0
       dac_gas_kelly.setVoltage(0,false);
       dac_bremse_kelly.setVoltage(abs(output),false);
       motor_control.state_foot_switch = 0;
@@ -359,18 +335,15 @@ void setup() {
   
   // initialze analog input pins
   pinMode(EXCITATION_CURRENT_POTI_ZOE_PIN,INPUT);
-  pinMode(GAS_POTI_ZOE_PIN,INPUT);
-  pinMode(BREMS_POTI_ZOE_PIN,INPUT);
+  pinMode(POTI_THROTTLE_DMC_PIN,INPUT);
+  pinMode(POTI_BRAKE_DMC_PIN,INPUT);
 
   // initialize digital output pins
   pinMode(PWM_ERREGUNG_ZOE_PIN,OUTPUT);
   digitalWrite(PWM_ERREGUNG_ZOE_PIN, LOW); 
   
   pinMode(FOOT_SWITCH_DMC_PIN,OUTPUT);
-  digitalWrite(FOOT_SWITCH_DMC_PIN, LOW); 
-  
-  pinMode(BRAKE_SWITCH_DMC_PIN,OUTPUT);
-  digitalWrite(BRAKE_SWITCH_DMC_PIN, LOW); 
+  digitalWrite(FOOT_SWITCH_DMC_PIN, LOW);  
 
   pinMode(FOOT_SWITCH_KELLY_PIN,OUTPUT);
   digitalWrite(FOOT_SWITCH_KELLY_PIN, LOW); 
@@ -417,7 +390,7 @@ void setup() {
   motor1_speed_pid.SetOutputLimits(0, double(motor_control_dmc_zoe.speed_max));
   motor1_speed_pid.SetMode(AUTOMATIC);
   
-  motor_control_dmc_zoe.excitation_current_max = round(power_supply.battery_voltage/R_ERREGERCOIL);
+  motor_control_dmc_zoe.excitation_current_max = round(power_supply.battery_voltage/R_EXCITATION_COIL);
   excitation_current_pid.SetOutputLimits(0, double(motor_control_dmc_zoe.excitation_current_max));
   excitation_current_pid.SetMode(AUTOMATIC);
 
